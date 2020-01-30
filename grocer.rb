a10b216579542345ead81decdd28a698f8c9b614
Alessandro Duterte <alexduterte@gmail.com>
2020-01-30 23:08:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,6 +71,16 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+
+  i = 0
+  while i < cart.length do
+    if cart[i][:clearance]
+      cart[i][:clearance] *= .8
+
+    end
+    i += 1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
